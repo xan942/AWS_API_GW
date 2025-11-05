1d089b1ecad665e40ea490a4f4ddf021a6240cd1
xan942 <xn39526@gmail.com>
2025-11-05 00:00:18 -0800

    update api endpoint

--- terraform/main.tf
+++ terraform/main.tf
@@ -44,10 +44,10 @@ data "archive_file" "lambda_zip" {
 resource "aws_lambda_function" "hello" {
   filename         = data.archive_file.lambda_zip.output_path
   function_name    = "${var.project_name}-hello"
-  role            = aws_iam_role.lambda_role.arn
-  handler         = "index.handler"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "index.handler"
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
-  runtime         = "nodejs18.x"
+  runtime          = "nodejs18.x"
 
   environment {
     variables = {
@@ -60,6 +60,14 @@ resource "aws_lambda_function" "hello" {
 resource "aws_apigatewayv2_api" "main" {
   name          = "${var.project_name}-api"
   protocol_type = "HTTP"
+
+  cors_configuration {
+    allow_credentials = false
+    allow_headers     = ["*"]
+    allow_methods     = ["GET", "OPTIONS"]
+    allow_origins     = ["*"]
+    max_age           = 86400
+  }
 }
 
 resource "aws_apigatewayv2_stage" "main" {
@@ -75,9 +83,10 @@ resource "aws_apigatewayv2_integration" "hello" {
 }
 
 resource "aws_apigatewayv2_route" "hello" {
-  api_id    = aws_apigatewayv2_api.main.id
-  route_key = "GET /hello"
-  target    = "integrations/${aws_apigatewayv2_integration.hello.id}"
+  api_id             = aws_apigatewayv2_api.main.id
+  route_key          = "GET /hello"
+  target             = "integrations/${aws_apigatewayv2_integration.hello.id}"
+  authorization_type = "AWS_IAM"
 }
 
 resource "aws_lambda_permission" "apigw" {
@@ -86,5 +95,38 @@ resource "aws_lambda_permission" "apigw" {
   function_name = aws_lambda_function.hello.function_name
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_apigatewayv2_api.main.execution_arn}/*/*"
+}
+
+# Optional Custom Domain for HTTP API
+resource "aws_apigatewayv2_domain_name" "custom" {
+  count       = var.enable_custom_domain ? 1 : 0
+  domain_name = var.custom_domain_name
+
+  domain_name_configuration {
+    certificate_arn = var.acm_certificate_arn
+    endpoint_type   = "REGIONAL"
+    security_policy = "TLS_1_2"
+  }
+}
+
+resource "aws_apigatewayv2_api_mapping" "custom" {
+  count           = var.enable_custom_domain ? 1 : 0
+  api_id          = aws_apigatewayv2_api.main.id
+  domain_name     = aws_apigatewayv2_domain_name.custom[0].domain_name
+  stage           = aws_apigatewayv2_stage.main.name
+  api_mapping_key = var.custom_domain_base_path
+}
+
+resource "aws_route53_record" "custom_domain" {
+  count   = var.enable_custom_domain && var.hosted_zone_id != "" ? 1 : 0
+  zone_id = var.hosted_zone_id
+  name    = var.custom_domain_name
+  type    = "A"
+
+  alias {
+    name                   = aws_apigatewayv2_domain_name.custom[0].domain_name_configuration[0].target_domain_name
+    zone_id                = aws_apigatewayv2_domain_name.custom[0].domain_name_configuration[0].hosted_zone_id
+    evaluate_target_health = false
+  }
 }
 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -18,3 +18,33 @@ variable "environment" {
   default     = "dev"
 }
 
+variable "enable_custom_domain" {
+  description = "Enable custom domain for the API"
+  type        = bool
+  default     = false
+}
+
+variable "custom_domain_name" {
+  description = "Custom domain name (e.g., api.example.com)"
+  type        = string
+  default     = ""
+}
+
+variable "acm_certificate_arn" {
+  description = "ACM certificate ARN for the custom domain (in the same region as the API)"
+  type        = string
+  default     = ""
+}
+
+variable "custom_domain_base_path" {
+  description = "Base path for API mapping (empty for root)"
+  type        = string
+  default     = ""
+}
+
+variable "hosted_zone_id" {
+  description = "Route53 Hosted Zone ID for your domain (to create an alias record)"
+  type        = string
+  default     = ""
+}
+
